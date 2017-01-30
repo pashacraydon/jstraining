a614f0fb9e7577e65d394312bf9ee2b4197f0464
Pashasc <pasha@safaribooksonline.com>
2017-01-30 11:20:06 -0800

    commit changes

--- es6stuff.js
+++ es6stuff.js
@@ -1,15 +1,27 @@
 
+const
+
+
 Object destructuring;
 
-es5;
-var cat = {
+
+
+
+let cat = {
     color: "grey",
     gender: "female",
     age: 2
   }
 
-es6;
+
+cat.color = 'black';
+
+
+
 var { color, gender } = cat
+
+
+
 color = "black"
 gender = "male"
 
@@ -25,15 +37,92 @@ console.log(cat.gender) // "female"
 
 Array destructuring;
 
+
+
+
 // pull out values from arrays and scope them to local variables
 const [firstAnimal] = ["cat", "dog", "lion", "horse"]
 console.log(firstAnimal) // "cat"
 
 // pass over values
 const [,,thirdAnimal,] = ["cat", "dog", "lion", "horse"]
-console.log(firstAnimal) // "lion"
+console.log(thirdAnimal) // "lion"
 
 
 // spread operator
 const [firstAnimal,, ...remaining] = ["cat", "dog", "lion", "horse"]
 console.log(firstAnimal, ...remaining) // "cat", ["lion", "horse"]
+
+function Animal (type) {
+  this.state = { 
+    'type': type
+  };
+}
+
+Animal.prototype.getAnimalType = function () {
+  console.log('The animal is a ' + this.state.type);
+}
+
+var animal = new Animal('dog');
+
+
+
+class Animal {
+  constructor (type) {
+    this.state = {
+      'type': type
+    }
+  }
+
+  getAnimalType() {
+    console.log(`The animal is a ${this.state.type}`);
+  }
+}
+
+const animal = new Animal('cat');
+
+
+const class Dog extends Animal {
+  constructor (type, breed) {
+    super(type);
+    this.breed = breed;
+  }
+
+  print() {
+    super.getAnimalType();
+    console.log(`The breed is ${this.breed}`);
+  }
+}
+
+
+const dog = new Dog('dog', 'samoyed');
+
+
+
+function dog (breed) {
+  console.log('Dog breed is ' + breed);
+}
+
+const dog = (breed) => {
+  console.log(`Dog breed is ${breed}`);
+}
+
+dog('samoyed');
+
+
+
+
+const export default print = () => {}
+import { print } from './helpers' 
+
+const print = () => {}
+import { print } from './helpers' 
+
+const export print = () => {}
+const export log = () => {}
+import * as h from './helpers';
+h.print();
+
+
+
+
